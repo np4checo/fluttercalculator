149d2610cabe6cfd26e3f5843a87c4be5aca5ee8
Natã Pacheco <natan.pacheco.1998@gmail.com>
2022-10-26 15:16:13 -0300

    number-implement

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,6 +13,33 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  String numero = 'Número';
+
+  void calcular(String tecla) {
+    switch (tecla) {
+      case 'AC':
+        setState(() {
+          numero = '0';
+        });
+        break;
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        setState(() {
+          numero += tecla;
+        });
+        break;
+
+      default:
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,20 +50,89 @@ class _MyAppState extends State<MyApp> {
         body: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            Text('Coluna 1'),
-            Text('Coluna 2'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  numero,
+                  style: const TextStyle(fontSize: 72),
+                )
+              ],
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Text('7'),
-                Text('8'),
-                Text('9'),
-                Text('/'),
+                GestureDetector(
+                    onTap: () {
+                      calcular('AC');
+                    },
+                    child: const Text('AC', style: TextStyle(fontSize: 48))),
+                const Text(''),
+                const Text(''),
+                const Text('<X', style: TextStyle(fontSize: 48)),
               ],
             ),
-            Text('Coluna 4'),
-            Text('Coluna 5'),
-            Text('Coluna 6'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                GestureDetector(
+                  onTap: () => calcular('7'),
+                  child: const Text(
+                    '7',
+                    style: TextStyle(fontSize: 48),
+                  ),
+                ),
+                GestureDetector(
+                    onTap: () => calcular('8'),
+                    child: const Text('8', style: TextStyle(fontSize: 48))),
+                GestureDetector(
+                    onTap: () => calcular('9'),
+                    child: const Text('9', style: TextStyle(fontSize: 48))),
+                const Text('/', style: TextStyle(fontSize: 48)),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                GestureDetector(
+                    onTap: () => calcular('4'),
+                    child: const Text('4', style: TextStyle(fontSize: 48))),
+                GestureDetector(
+                    onTap: () => calcular('5'),
+                    child: const Text('5', style: TextStyle(fontSize: 48))),
+                GestureDetector(
+                    onTap: () => calcular('6'),
+                    child: const Text('6', style: TextStyle(fontSize: 48))),
+                const Text('X', style: TextStyle(fontSize: 48)),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                GestureDetector(
+                    onTap: () => calcular('1'),
+                    child: const Text('1', style: TextStyle(fontSize: 48))),
+                GestureDetector(
+                    onTap: () => calcular('2'),
+                    child: const Text('2', style: TextStyle(fontSize: 48))),
+                GestureDetector(
+                    onTap: () => calcular('3'),
+                    child: const Text('3', style: TextStyle(fontSize: 48))),
+                const Text('-', style: TextStyle(fontSize: 48)),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                GestureDetector(
+                    onTap: () => calcular('0'),
+                    child: const Text('0', style: TextStyle(fontSize: 48))),
+                const Text(',', style: TextStyle(fontSize: 48)),
+                const Text('=', style: TextStyle(fontSize: 48)),
+                const Text('+', style: TextStyle(fontSize: 48)),
+              ],
+            ),
+            const Text('Coluna 6')
           ],
         ),
       ),
